5ede807a46ba4f6b4da9efd30d396656bad68394
Ygor <ygor.20260279@institutojef.org.br>
2026-08-19 10:27:53 -0300

    Adicionei comentários

--- src/main/java/org/astro/astro_app/model/Alerta.java
+++ src/main/java/org/astro/astro_app/model/Alerta.java
@@ -1,17 +1,23 @@
 package org.astro.astro_app.model;
+
+//Classe model Alerta
 public class Alerta {
+    //Declaração de variáveis
+
+    //Atributos da classe
     private int codigo;
     private int dtLimite;
     private String descricao;
     private int idEmpresa;
 
+    //Mètodo construtor da classe
     public Alerta(int codigo, int dtLimite, String descricao, int idEmpresa) {
         this.codigo = codigo;
         this.dtLimite = dtLimite;
         this.descricao = descricao;
         this.idEmpresa = idEmpresa;
     }
-
+    //Métodos Getters
     public int getCodigo() {
         return codigo;
     }
